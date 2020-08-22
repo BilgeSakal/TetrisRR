131825a76d144277a6f9327192052efdde95c409
Nuri Furkan Pala <nrfurkan@gmail.com>
2020-08-22 09:10:58 +0300

    Revert "A comment has been added for try."
    This reverts commit a8351a20e0d5149f88c0716de8f2b15d72554a39.

--- src/Piece.java
+++ src/Piece.java
@@ -3,7 +3,6 @@ public class Piece {
 
 	public void rotate(TetrisDirectionsEnum direction) {
 		// TODO implement this method
-		// henüz düþünmedik
 	}
 
 	public Piece move(TetrisDirectionsEnum direction) {
