a8351a20e0d5149f88c0716de8f2b15d72554a39
nuri <nuri@DESKTOP-0K59NJ8>
2020-08-22 09:04:45 +0300

    A comment has been added for try.

--- src/Piece.java
+++ src/Piece.java
@@ -3,6 +3,7 @@ public class Piece {
 
 	public void rotate(TetrisDirectionsEnum direction) {
 		// TODO implement this method
+		// henüz düþünmedik
 	}
 
 	public Piece move(TetrisDirectionsEnum direction) {
